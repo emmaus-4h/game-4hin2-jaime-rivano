ac8d230572f2e7ac3e151f6331b80b8d163f3a6e
Rivano69 <93260697+RivanoEngeldal69@users.noreply.github.com>
2022-05-06 14:31:09 +0000

    jndf

--- script.js
+++ script.js
@@ -14,11 +14,14 @@ const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
-var spelerX = 600; // x-positie van speler
+var spelerX = 350; // x-positie van speler
 var spelerY = 600; // y-positie van speler
 
 var vijandX = 300; // x-positie van speler
 var vijandY = 600; // y-positie van speler
+
+var kogelX = 400; // x-positie van speler
+var kogelY = 600; // y-positie van speler
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -74,26 +77,57 @@ var tekenAlles = function () {
   // achtergrond
   fill("green");
   rect(0,0,1280,720);
-  // vijand / Dwayne Johnson
- fill (0, 0, 0);
-  ellipse (vijandX, vijandY - 100, 50, 50); // midden
-  // kogel / Steen
+  // vijand / Dwayne Johnson 
+  noStroke();
+  fill (140, 100, 77);
+  rect(vijandX-12, vijandY +12, 24, 26); // benen
 
-  // speler / Ryan Reynolds
-  fill (255, 255, 255);
-  rect(spelerX, spelerY +40 - 100, 1, 25); // pijpstreep
-  fill (0, 0, 0);
-  rect(spelerX-11, spelerY +35 - 100, 22, 25); // broek
-  fill (255,255,255);
-  rect(spelerX-12, spelerY +10 - 100, 24, 25); // shirt
-  fill (222, 180, 151);
-  ellipse(spelerX, spelerY - 100, 20, 20); // hoofd
+    fill (50, 50, 50);
+  rect(vijandX -1, vijandY +19, 2, 19); // dubbelbeen
+  
+  fill (140, 100, 77);
+  rect(vijandX-12, vijandY-13, 24, 26); // torso
+
+  fill (0,0,0);
+  triangle(vijandX -12, vijandY +11, vijandX +12, vijandY +11, vijandX, vijandY +20); // speedo
+
+  fill (140, 100, 77);
+  rect(vijandX-20, vijandY-13, 7, 24); // linkerarm
+
+  fill (140, 100, 77);
+  rect(vijandX+13, vijandY-13, 7, 24); // rechterarm
+  
+  fill (140, 100, 77);
+  ellipse(vijandX, vijandY -23, 20, 20); // hoofd
   
   fill (0, 0, 0);
-  ellipse (spelerX, spelerY - 100, 5, 5); // midden
+  ellipse (vijandX, vijandY, 5, 5); // midden
+
+  // speler / Ryan Reynolds
+  noStroke();
+
+  fill (255,255,255);
+  rect(spelerX-12, spelerY-13, 24, 26); // torso
+  
+  fill (0, 0, 0);
+  rect(spelerX-12, spelerY +11, 24, 26); // benen
+
+    fill (50, 50, 50);
+  rect(spelerX -1, spelerY +19, 2, 18); // dubbelbeen
+
+  fill (255,255,255);
+  rect(spelerX-18, spelerY-13, 5, 24); // linkerarm
+
+  fill (255,255,255);
+  rect(spelerX+13, spelerY-13, 5, 24); // rechterarm
+  
+  fill (222, 180, 151);
+  ellipse(spelerX, spelerY -23, 20, 20); // hoofd
+  
+  fill (0, 0, 0);
+  ellipse (spelerX, spelerY, 5, 5); // midden
 
   // kogel / Vuurbal
-  fill
   
   // punten en health
 

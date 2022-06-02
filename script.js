97c1a6a2c7918e7d4809ceaf15819aeae0b1083c
Rivano69 <93260697+RivanoEngeldal69@users.noreply.github.com>
2022-06-02 21:55:10 +0000

    armen oa

--- script.js
+++ script.js
@@ -12,9 +12,10 @@
 
 const SPELEN = 1;
 const GAMEOVER = 2;
-const START = 3;
-const LEVEL = 4;
-var spelStatus = START;
+const LEVEL = 3;
+const INTRO = 4;
+const UITLEG = 5;
+var spelStatus = INTRO;
 
 
 var vijandX = 400; // x-positie van vijand
@@ -60,21 +61,26 @@ var kogelSnelheid = 2;
 var targetX;
 var targetY;
 
-var fireX;
-var fireY;
-  
-  if (keyIsDown(32) && kogelVliegt === false) {
+var schietPlaatsX;
+var schietPlaatsY;
+
+var spaceDownNu = false;
+var spaceDownLast = false;
+
+  spaceDownLast = spaceDownNu;
+  spaceDownNu = keyIsDown(32);
+  if (spaceDownLast === false && spaceDownNu === true && kogelVliegt === false) {
     kogelVliegt = true;
     targetX = mouseX;
     targetY = mouseY;
-    kogelX = fireX;
-    kogelY = fireY;
-    fireX = spelerX;
-    fireY= spelerY;
+    kogelX = schietPlaatsX;
+    kogelY = schietPlaatsY;
+    schietPlaatsX = spelerX;
+    schietPlaatsY= spelerY;
   }
 
-  var richtingX = targetX - fireX;
-  var richtingY = targetY - fireY;
+  var richtingX = targetX - schietPlaatsX;
+  var richtingY = targetY - schietPlaatsY;
 
   var correctieSnelheid = Math.sqrt(((richtingX * richtingX)+ (richtingY * richtingY))) / 1.412
 
@@ -112,7 +118,19 @@ var verwerkBotsing = function () {
   }
   // botsing kogel tegen vijand
   
-  // update punten en health
+  // bomen
+    if (spelerX > 100) {
+    spelerX = spelerX -2;
+  }
+  if (spelerX < 1170) {
+    spelerX = spelerX +2;
+  }
+  if (spelerY < 150) {
+    spelerY = spelerY +2;
+  }
+  if (spelerY > 575) {
+    spelerY = spelerY -2;
+  }
 
 };
 
@@ -121,30 +139,36 @@ var verwerkBotsing = function () {
  */
 var tekenAlles = function () {
   // achtergrond
-  fill(235, 205, 122);
-  rect(0,0,1280,720);
-  
-  //plaatjes
+  image(img6, 0, 0, 1280, 720)
 
   // speler / Ryan Reynolds
   noStroke();
   fill (242, 204, 183);
-  rect(spelerX-5, spelerY-28, 10, 33); // nek
+  rect(spelerX -5, spelerY -28, 10, 33); // nek
   
   fill (255,255,255);
-  rect(spelerX-14, spelerY-15, 28, 30); // torso
+  rect(spelerX -14, spelerY -15, 28, 30); // torso
   
   fill (0, 0, 0);
-  rect(spelerX-14, spelerY +15, 28, 30); // benen
+  rect(spelerX -14, spelerY +15, 28, 30); // benen
 
   fill (50, 50, 50);
   rect(spelerX -1, spelerY +22, 2, 23); // dubbelbeen
 
+  if (keyIsDown(65)) {
   fill (255,255,255);
-  rect(spelerX-22, spelerY-13, 7, 33); // linkerarm
+  rect(spelerX -48, spelerY -13, 33, 7); // if linkerarm
 
   fill (255,255,255);
-  rect(spelerX+15, spelerY-13, 33, 7); // rechterarm
+  rect(spelerX +22, spelerY -13, -7, 33); // if rechterarm
+  } else {
+  
+  fill (255,255,255);
+  rect(spelerX-22, spelerY-13, 7, 33); // else linkerarm
+
+  fill (255,255,255);
+  rect(spelerX+15, spelerY-13, 33, 7); // else rechterarm
+  }
 
   image(img1, spelerX -17, spelerY -59, 35, 42); // Ryan
   
@@ -155,19 +179,19 @@ var tekenAlles = function () {
   var vijand = function() {
     noStroke();
   fill (140, 100, 77);
-  rect(vijandX-14, vijandY-20, 28, 65); // torso
+  rect(vijandX -14, vijandY -20, 28, 65); // torso
   
   fill (50, 50, 50);
   rect(vijandX -1, vijandY +22, 2, 23); // dubbelbeen
 
   fill (140, 100, 77);
-  rect(vijandX-24, vijandY-18, 9, 33); // linkerarm
+  rect(vijandX -24, vijandY -18, 9, 33); // linkerarm
 
   fill (140, 100, 77);
-  rect(vijandX+15, vijandY-18, 9, 33); // rechterarm
+  rect(vijandX +15, vijandY -18, 9, 33); // rechterarm
 
   fill (140, 100, 77);
-  rect(vijandX-5, vijandY-28, 10, 33); // nek
+  rect(vijandX -5, vijandY -28, 10, 33); // nek
   
   image(img2, vijandX -13, vijandY -63, 35, 42); // Dwayne
 
@@ -207,6 +231,7 @@ function preload (){ //plaatjes
   img3 = loadImage("afbeeldingen/Intro.jpeg")
   img4 = loadImage("afbeeldingen/Over.jpeg")
   img5 = loadImage("afbeeldingen/Uitleg.jpeg")
+  img6 = loadImage("afbeeldingen/Achter.jpg")
 }
 /**
  * setup
@@ -233,28 +258,47 @@ function draw() {
     verwerkBotsing();
     tekenAlles();
   }
+  
   if (spelStatus === GAMEOVER) {
     console.log("game over")
      image(img4, 0, 0, 1280, 720)
     if(keyIsDown(32)){
       spelerX=640;
-      spelStatus = START;
+      spelStatus = INTRO;
     }
   }
 
-  if (spelStatus === START) {
-    console.log("start")
-    image(img3, 0, 0, 1280, 720)
-    if(keyIsDown(32)){
-      spelerX=640;
-      spelStatus = SPELEN;
-    }
-  }
   if (spelStatus === LEVEL) {
     console.log("LEVEL += 1")
     beweegAlles();
     verwerkBotsing();
     tekenAlles();
     (vijand = vijand +1);
+  }
+
+  if (spelStatus === INTRO) {
+    console.log("intro")
+    image(img3, 0, 0, 1280, 720)
+    if(mouseIsPressed && 
+       mouseX >60 && mouseX <335 &&
+       mouseY >340 && mouseY <410){
+      spelerX=640;
+      spelStatus = SPELEN;
+    }
+    if(mouseIsPressed &&
+       mouseX >940 && mouseX <1245 &&
+       mouseY >340 && mouseY <400){
+      spelStatus = UITLEG;
+    }
+  }
+  
+  if (spelStatus === UITLEG) {
+    console.log("uitleg")
+    image(img5, 0, 0, 1280, 720)
+    if(mouseIsPressed &&
+       mouseX >530 && mouseX <775 &&
+       mouseY >615 && mouseY <670){
+      spelStatus = INTRO;
+    }
   }
 }
